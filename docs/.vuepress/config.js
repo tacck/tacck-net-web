f2694aec4d572b59331353b0efa300089c994afc
Takuya KIHARA <gray@tacck.net>
2019-06-30 12:33:11 +0900

    :sparkles: サイドバー設定

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,17 @@
 module.exports = {
     title: 'Tacck.NET',
     description: 'tacck.net',
-    ga: 'UA-89701884-5'
+    ga: 'UA-89701884-5',
+    themeConfig: {
+        lastUpdated: 'Last Updated (JST)',
+        nav: [
+            { text: 'Home', link: '/' },
+        ],
+        sidebarDepth: 2,
+        sidebar: [
+            '/about',
+            '/works',
+            ['https://blog.tacck.net', 'Tech Blog']
+        ]
+    }
 }
