2086989cc6b0e251d6a05af81d260274d5eed618
Takuya KIHARA <gray@tacck.net>
2019-12-14 14:41:49 +0900

    :pencil: updated

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,18 @@
 module.exports = {
-    title: 'Tacck.NET',
-    description: 'tacck.net',
-    ga: 'UA-89701884-5',
-    themeConfig: {
-        lastUpdated: 'Last Updated (JST)',
-        nav: [
-            { text: 'Home', link: '/' },
-        ],
-        sidebarDepth: 2,
-        sidebar: [
-            '/about',
-            '/works',
-            ['https://blog.tacck.net', 'Tech Blog']
-        ]
-    }
+  title: 'Tacck.NET',
+  description: 'tacck.net',
+  ga: 'UA-89701884-5',
+  themeConfig: {
+    lastUpdated: 'Last Updated (JST)',
+    nav: [
+      { text: 'Home', link: '/' },
+    ],
+    sidebar: [
+      '/',
+      '/about',
+      '/works',
+      ['https://meetup.tacck.net/', 'Meetups'],
+      ['https://blog.tacck.net', 'Tech Blog']
+    ]
+  }
 }
